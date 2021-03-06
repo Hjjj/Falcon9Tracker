60c2cbf35a255f20aec969cd0309c0358216c080
hal <wenonaliar@yahoo.com>
2021-03-06 01:36:43 -0700

    Refine stepper code

--- stepper_test.py
+++ stepper_test.py
@@ -6,16 +6,13 @@ STEP_PIN = 21
 DIR_PIN = 20
 ENABL_PIN = 16
 
-ENABLE_Y = 0
-ENABLE_N = 1
-
 #160 steps in one revolution at current settings
 STEPS_PER_REVOLUTION = 160
 
 #on =0 because pin low enables board
-class OnOff(IntEnum):
-    ON = 0 
-    OFF = 1
+class EnabledState(IntEnum):
+    ENABLED = 0 
+    DISABLED = 1
 
 class Dir(IntEnum):
     CLOCKWISE = 0
@@ -27,19 +24,19 @@ def init_power_pins():
     GPIO.setmode(GPIO.BCM)
 
     GPIO.setup(STEP_PIN, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(DIR_PIN, GPIO.OUT, initial=int(Dir.CLOCKWISE))
+    GPIO.setup(DIR_PIN, GPIO.OUT, initial=Dir.CLOCKWISE)
     #keep the stepper board off until ready to start
-    GPIO.setup(ENABL_PIN, GPIO.OUT, initial=ENABLE_N)
+    GPIO.setup(ENABL_PIN, GPIO.OUT, initial=EnabledState.DISABLED)
 
 def shutdown_driver_board():
     GPIO.output(STEP_PIN, GPIO.LOW)
     GPIO.output(DIR_PIN, GPIO.LOW)
-    GPIO.output(ENABL_PIN, ENABLE_N)  
+    GPIO.output(ENABL_PIN, EnabledState.DISABLED)  
 
 def rotate_n(rotations, speed, dir):
 
     counter = rotations * STEPS_PER_REVOLUTION
-    GPIO.output(DIR_PIN, int(dir))
+    GPIO.output(DIR_PIN, dir)
     
     while counter >= 0 :
         
@@ -48,16 +45,16 @@ def rotate_n(rotations, speed, dir):
         GPIO.output(STEP_PIN, 0)
         counter = counter - 1
 
-def toggle_motor(onoff):
-    GPIO.output(ENABL_PIN, int(onoff))
+def enable_stepper_board(onoff):
+    GPIO.output(ENABL_PIN, onoff)
 
 #MAIN CODE
 
 init_power_pins()
-toggle_motor(OnOff.ON)
+enable_stepper_board(EnabledState.ENABLED)
 
 #.0005 is close to max speed i've found
-STEP_DELAY = 0.0005 # 0.0005
+STEP_DELAY = 0.0008 # 0.0005
 #time.sleep(3)
 
 rotate_n(10, STEP_DELAY, Dir.CLOCKWISE)
@@ -73,7 +70,7 @@ time.sleep(.5)
 rotate_n(10, STEP_DELAY, Dir.COUNTER)
 
 time.sleep(1)
-toggle_motor(OnOff.OFF)
+enable_stepper_board(EnabledState.DISABLED)
 
 shutdown_driver_board()
 
